2497ed05e8fe156be98c0170a13d5e39906c30b7
Rachel Sanders <rsanders@linkedin.com>
2014-10-08 18:56:42 -0700

    Adding Heroku files

--- code24hours/code24hours/wsgi.py
+++ code24hours/code24hours/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for code24hours project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "code24hours.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "code24hours.settings.local")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
