4f6e09523dcaf010776bc385dcec2900de04ca1b
Rachel Sanders <rsanders@linkedin.com>
2014-10-09 10:53:55 -0700

    Disabling Django admin interface for now, don't need it

--- code24hours/urls.py
+++ code24hours/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'code24hours.teaser.views.placeholder', name='placeholder'),
-    #url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # disable admin urls for now, don't need it
+    # url(r'^admin/', include(admin.site.urls)),
 )
